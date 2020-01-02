825ef2ec2a7152148b803eb22103fe60b51f1a02
rafaelddem <42066475+rafaelddem@users.noreply.github.com>
2020-01-02 13:43:02 -0300

    Removido javascript index

--- index.php
+++ index.php
@@ -3,20 +3,6 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Título</title>
-		<script language="javascript" type="text/javascript">
-			function ajaxFunction(param){
-				var ajaxRequest;  // The variable that makes Ajax possible!
-				ajaxRequest = new XMLHttpRequest();
-				ajaxRequest.onreadystatechange = function(){
-					if(ajaxRequest.readyState == 4){
-						var ajaxDisplay = document.getElementById('senha');
-						ajaxDisplay.innerHTML = ajaxRequest.responseText;
-					}
-				}
-				ajaxRequest.open("GET", "view/view_cliente.php", true);
-				ajaxRequest.send(null);
-			}
-		</script>
 	</head>
 	<body>
 		<form name="form_terminal" id="form_terminal" action="controller\contr_terminal.php" method="post">
